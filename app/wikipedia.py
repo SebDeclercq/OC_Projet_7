e756b670886f585018a97adba76e59956534e22a
SebDeclercq <declercq_sebastien@yahoo.fr>
2019-01-26 15:56:16 +0100

    Wikipedia API : from start to end OK

--- app/wikipedia.py
+++ app/wikipedia.py
@@ -1,16 +1,38 @@
 #!/usr/bin/env python3
+'''
+@note       Module containing the class querying the Wikipedia API.
+@author     Sébastien Declercq <sdq@afnor.org>
+@version    0.0.1 (2019-01-24) : init
+'''
 from typing import List
 import mediawiki
 from app.google_maps import Position
 
 
 class Wikipedia:
+    '''Class connecting to the Wikipedia API and offers a search
+    method to retrieve a MediaWikiPage content'''
     def __init__(self) -> None:
-        self.client: mediawiki.mediawiki.MediaWiki = mediawiki.MediaWiki()
-
-    def geosearch(self, position: Position) -> List[str]:
-        return self.client.geosearch(
-            str(position.latitude), str(position.longitude)
+        '''Constructor'''
+        self.client: mediawiki.mediawiki.MediaWiki = mediawiki.MediaWiki(
+            lang='fr'
         )
 
-    
+    def geosearch(self, position: Position) -> List[str]:
+        '''Performs a geosearch on the Wikipedia API
+        Params:
+            position: used to get GPS data for the search
+        Returns:
+            A list of results (title pages from wikipedia)'''
+        return self.client.geosearch(
+            str(position.latitude), str(position.longitude),
+            results=3
+        )
+
+    def page_search(self, title: str) -> mediawiki.MediaWikiPage:
+        '''Performs a page search on the Wikipedia API
+        Params:
+            title: the title of the page to collect
+        Returns:
+            An instance of MediaWikiPage'''
+        return self.client.page(title)
--- tests/unit/test_app_wikipedia.py
+++ tests/unit/test_app_wikipedia.py
@@ -7,23 +7,75 @@
 @version    0.0.1 (2019-01-20) : init
 '''
 from typing import List, Sequence, Tuple
-from unittest import mock
+import mediawiki
 import pytest
 import app.wikipedia
 from app.google_maps import Position
 
+
+@pytest.fixture
+def wikipedia() -> app.wikipedia.Wikipedia:
+    '''Fixture instanciating a Wikipedia object'''
+    return app.wikipedia.Wikipedia()
+
+
 class Params:
     '''Class holding params for parametrized tests'''
     geosearch: Sequence[Tuple[Position, List[str]]] = (
-        (Position(50.2934211, 2.7787176), ['Arras', 'Hôtel de Ville, Arras']),
+        (Position(50.2934211, 2.7787176), [
+            'Hôtel Les Trois Luppars',
+            "Grand-Place d'Arras",
+            'Chapelle des Chariottes',
+        ]),
+        (Position(50.2911244, 2.7769304), [
+            "Beffroi d'Arras",
+            "Hôtel de ville d'Arras",
+            'Place des Héros (Arras)',
+        ]),
+        (Position(50.2724255, 2.7561987), ['Achicourt', "Gare d'Achicourt"]),
+        (Position(50.6409299, 3.0445812), [
+            'Citadelle de Lille',
+            'Les Poussins, Parc de la Citadelle',
+            'Esplanade du Champ de Mars (Lille)'
+        ]),
+        (Position(50.2827966, 2.7600242), [
+            'Main Square Festival',
+            "Citadelle d'Arras", "Faubourg d'Amiens British Cemetery, ",
+            "The Arras Mémorial And The Flying Services Mémorial"
+        ]),
     )
+    page_search: Sequence[Tuple[str, str]] = (
+        ("Hôtel Les Trois Luppars",
+         '''L'hôtel Les Trois Luppars est un établissement hô'''),
+        ("Beffroi d'Arras",
+         '''Le beffroi d'Arras est un ouvrage de charpente de'''),
+        ("Achicourt",
+         '''Achicourt est une commune française située dans l'''),
+        ("Citadelle de Lille",
+         '''La citadelle de Lille est un ouvrage militaire bâ'''),
+        ("Main Square Festival",
+         '''Le Main Square Festival est un festival de musiqu'''),
+    )
+
 
 class TestWikipedia:
     '''Class testing the app.wikipedia.Wikipedia'''
     @pytest.mark.parametrize('position, expected_list', Params.geosearch)
     def test_wikipedia_geosearch(
-            self, position: Position, expected_list: List[str]
+            self, position: Position, expected_list: List[str],
+            wikipedia: app.wikipedia.Wikipedia
     ) -> None:
-        wikipedia: app.wikipedia.Wikipedia = app.wikipedia.Wikipedia()
+        '''Checks that Wikipedia returns the exact expected results (3)'''
         list_results: List[str] = wikipedia.geosearch(position)
-        assert list_results[:2] == expected_list
+        assert list_results == expected_list
+
+    @pytest.mark.parametrize('title, summary_50_char', Params.page_search)
+    def test_wikipedia_get_page(
+            self, title: str, summary_50_char: str,
+            wikipedia: app.wikipedia.Wikipedia
+    ) -> None:
+        '''Checks that the page search returns the expected summary
+        (50 first characters)'''
+        page = wikipedia.page_search(title)
+        assert isinstance(page, mediawiki.MediaWikiPage)
+        assert page.summary[:49] == summary_50_char
